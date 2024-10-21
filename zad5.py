86d73e463cb595529260712d93f3030ce4121dfe
Myszon47 <123365113+Myszon47@users.noreply.github.com>
2024-10-21 16:28:04 +0200

    Delete zad5.py

--- zad5.py
+++
@@ -1,9 +0,0 @@
-from zad3 import exponentation
-from gmpy2 import mpz, f_div
-
-def sqrt_modulo_p(b,p):
-	exp = f_div((p+mpz(1)), mpz(4))
-	return exponentation(b,exp,p)
-
-
-print(sqrt_modulo_p(mpz(5),mpz(11)))
